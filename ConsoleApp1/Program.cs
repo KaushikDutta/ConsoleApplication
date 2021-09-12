7403b875b70621932a664f411f34182c93c77c70
Kaushik <kdutta_2001@yahoo.com>
2021-09-12 10:26:25 -0400

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Kaushik";
+            StringBuilder sb = new StringBuilder();
+            sb.Append(name);
+
+            Console.WriteLine("Hello World " + sb.ToString() + "!");
+        }
+    }
+}
